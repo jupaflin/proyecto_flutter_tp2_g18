047f825b73530f0a1e08047d14663a1c7ec81a79
Matias Cappa <cappa.nicolas1@gmail.com>
2024-11-28 14:22:07 -0300

    boton de darkmode en la pagina de inicio

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,9 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_application_base/widgets/drawer_menu.dart';
+import 'package:flutter_application_base/helpers/preferences.dart';
+import 'package:flutter_application_base/providers/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -16,7 +19,37 @@ class HomeScreen extends StatelessWidget {
         toolbarHeight: 80,
       ),
       drawer: DrawerMenu(),
-      body: const Center(child: Text('Hola mundo')),
+      body: const Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(15.0),
+              child: DarkModeSwitchButton(),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Center(
+              child: Text("TP2",
+                style: TextStyle(
+                    fontSize: 24,                   
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Center(
+              child: Text(
+                "GRUPO 18",
+                style: TextStyle(
+                    fontSize: 24,      
+                  ),
+                ),
+            ),
+          ],
+        )
+      ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.plus_one),
         onPressed: () {
@@ -26,4 +59,37 @@ class HomeScreen extends StatelessWidget {
       floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
     );
   }
+}
+
+class DarkModeSwitchButton extends StatefulWidget {
+  const DarkModeSwitchButton({Key? key}) : super(key: key);
+
+  @override
+  State<DarkModeSwitchButton> createState() => _BodyProfileState();
+} 
+
+class _BodyProfileState extends State<DarkModeSwitchButton> {
+  @override
+  Widget build(BuildContext context) {
+    final temaProvider = Provider.of<ThemeProvider>(context, listen: false);
+    print(Preferences.darkmode);
+    return Column(
+      children: [
+        SwitchListTile.adaptive(
+          title: const Text('Dark Mode'),
+          value: Preferences.darkmode,
+          onChanged: (bool value) {
+            setState(() {
+              Preferences.darkmode = value; 
+              value ? temaProvider.setDark() : temaProvider.setLight();
+            });
+          },
+        ),
+        const SizedBox(
+          height: 15,
+        ),
+      ],
+    );
+  }
+  
 }
--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -20,8 +20,8 @@ class ProfileScreen extends StatelessWidget {
         child: Column(
           children: [
             HeaderProfile(size: size),
-            Padding(
-              padding: const EdgeInsets.all(15.0),
+            const Padding(
+              padding: EdgeInsets.all(15.0),
               child: BodyProfile(),
             ),
           ],
